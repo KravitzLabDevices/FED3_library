2516bb3c77646e8615f1caddaaf9d404616e0fcf
Kravitz Lab <alexxai@wustl.edu>
2020-12-24 12:09:58 -0600

    Added free feeding example

---
+++ examples/FreeFeeding/FreeFeeding.ino
@@ -0,0 +1,35 @@
+/*
+  Feeding experimentation device 3 (FED3)
+  Free Feeding
+  
+  alexxai@wustl.edu
+  December, 2020
+*/
+
+#include <FED3.h>                                       //Include the FED3 library 
+String sketch = "FreeFeed";                                  //Unique identifier text for each sketch
+FED3 fed3 (sketch);                                     //Start the FED3 object
+
+void setup() {
+  fed3.begin();                                         //Setup the FED3 hardware
+  fed3.FEDmode = 0;                                     //Customize the display options to FEDmode 1 for an operant session
+  fed3.EnableSleep = true;                             //Set to false to inhibit sleeping to use the Serial port; Set to true to reduce battery power
+}
+
+void loop() {
+  fed3.run();                                           //Call fed.run at least once per loop
+}
+
+////////////////////////////////////////////////////
+// Display data via the serial monitor (click Tools > Serial Monitor)
+////////////////////////////////////////////////////
+void serialOutput() {
+  Serial.println(" ");
+  Serial.println ("FreeFeeding");
+  Serial.print ("Unixtime is: ");
+  Serial.println(fed3.unixtime);
+  Serial.println("Pellets");
+  Serial.print("   ");
+  Serial.print(fed3.PelletCount);
+  Serial.println("**********************************");
+}

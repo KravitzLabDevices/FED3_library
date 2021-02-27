e0d984db8990a75fcbcc7b901ab2e26dcef5c5c8
Filipe Carvalho <34546039+filcarv@users.noreply.github.com>
2021-02-27 12:17:35 +0000

    Increased temporary resolution and added debounces.

--- src/FED3.h
+++ src/FED3.h
@@ -127,7 +127,7 @@ class FED3 {
         void FED3MenuScreen();
         
         //BNC input/output
-        void ReadBNC();
+		void ReadBNC(bool blinkGreen);
         bool BNCinput = false;
         
         // Motor
@@ -150,7 +150,7 @@ class FED3 {
         void logLeftPoke();
         void logRightPoke();
         void Feed();
-        void dispenseTimer();
+        bool dispenseTimer_ms(int ms);
         void pelletTrigger();
         void leftTrigger();
         void rightTrigger();
@@ -159,10 +159,10 @@ class FED3 {
         int minPokeTime = 0;
         
         //jam movements
-        void ClearJam();
-        void VibrateJam();
-        void MinorJam();
 		bool RotateDisk(int steps);
+        bool ClearJam();
+        bool VibrateJam();
+        bool MinorJam();
 
         //timed feeding variables
         int timedStart; //hour to start the timed Feeding session, out of 24 hour clock

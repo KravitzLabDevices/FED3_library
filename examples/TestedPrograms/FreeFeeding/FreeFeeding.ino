b0bb1fbc8973a6b90e274dc7caa51da0ea429e16
Kravitz Lab <alexxai@wustl.edu>
2021-03-11 23:06:32 -0600

    reorganized examples

---
+++ examples/TestedPrograms/FreeFeeding/FreeFeeding.ino
@@ -0,0 +1,27 @@
+/*
+  Feeding experimentation device 3 (FED3)
+  Free Feeding mode
+
+  alexxai@wustl.edu
+  December, 2020
+
+  This project is released under the terms of the Creative Commons - Attribution - ShareAlike 3.0 license:
+  human readable: https://creativecommons.org/licenses/by-sa/3.0/
+  legal wording: https://creativecommons.org/licenses/by-sa/3.0/legalcode
+  Copyright (c) 2020 Lex Kravitz
+*/
+
+#include <FED3.h>                                       //Include the FED3 library 
+String sketch = "FreeFeed";                             //Unique identifier text for each sketch (this will show up on the screen and in log file)
+FED3 fed3 (sketch);                                     //Start the FED3 object
+
+void setup() {
+  fed3.begin();                                         //Setup the FED3 hardware
+  fed3.DisplayPokes = false;                            //Customize the DisplayPokes option to 'false' to not display the poke indicators
+  fed3.timeout = 10;                                    //Set a timeout period (in seconds) after each pellet is taken
+}
+
+void loop() {
+  fed3.run();                                           //Call fed.run at least once per loop
+  fed3.Feed();                                          //Drop pellet
+}

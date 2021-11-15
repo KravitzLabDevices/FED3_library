ad10097cc2a5b115d9f5777cf6101ba02c7271b8
Kravitz Lab <alexxai@wustl.edu>
2021-11-15 09:59:33 -0600

    Revert "Merge pull request #39 from KravitzLabDevices/revert-38-main"
    This reverts commit 01dfd2682213dbb5f6c5d9e20bedf89da1f47982, reversing
    changes made to ba199dcf87279dd51a66af30ae9faba5675bfa19.

--- src/FED3.cpp
+++ src/FED3.cpp
@@ -74,8 +74,6 @@ void FED3::logLeftPoke(){
     leftInterval = 0.0;
     while (digitalRead (LEFT_POKE) == LOW) {}  //Hang here until poke is clear
     leftInterval = (millis()-leftPokeTime);
-    UpdateDisplay();
-    DisplayLeftInt();
     if (leftInterval < minPokeTime) {
       Event = "LeftShort";
     }
@@ -83,6 +81,8 @@ void FED3::logLeftPoke(){
       Event = "Left";
     }
     logdata();
+    UpdateDisplay();
+    DisplayLeftInt();
     Left = false;
   }
 }
@@ -95,8 +95,6 @@ void FED3::logRightPoke(){
     rightInterval = 0.0;
     while (digitalRead (RIGHT_POKE) == LOW) {} //Hang here until poke is clear
     rightInterval = (millis()-rightPokeTime);
-    UpdateDisplay();
-    DisplayRightInt();
     if (rightInterval < minPokeTime) {
       Event = "RightShort";
     }
@@ -104,6 +102,8 @@ void FED3::logRightPoke(){
       Event = "Right";
     }
     logdata();
+    UpdateDisplay();
+    DisplayRightInt();
     Right = false; 
   }
 }
@@ -291,6 +291,9 @@ bool FED3::VibrateJam() {
 //full rotation to clear jam
 bool FED3::ClearJam() {
     DisplayJamClear();
+    if (serialOn) {
+      jamAlertUpdate();
+    }
 	
 	if (dispenseTimer_ms(250)) {
 	  display.fillRect (5, 15, 120, 15, WHITE);  //erase the "Jam clear" text without clearing the entire screen by pasting a white box over it
@@ -881,8 +884,87 @@ void FED3::writeConfigFile() {
   configfile.close();
 }
 
+//Format a string s (recommended size at least 100) for sending over serial or logging to sd card
+void FED3::writeDataString(char* s, DateTime now){
+  char activePokeStr[6];
+  if (activePoke == 0){
+    strcpy(activePokeStr, "Right");
+  } else {
+    strcpy(activePokeStr, "Left");
+  }
+  
+  char retIntervalStr[10];
+  if (Event != "Pellet"){
+    strcpy(retIntervalStr, "nan"); // print NaN if it's not a pellet Event
+  }
+  else if (retInterval < 60000 ) {  // only log retrieval intervals below 1 minute (FED should not record any longer than this)
+    sprintf(retIntervalStr, "%.2f", retInterval/1000.0); // print interval between pellet dispensing and being taken
+  }
+  else if (retInterval >= 60000) {
+    strcpy(retIntervalStr, "Timed_out"); // print "Timed_out" if retreival interval is >60s
+  }
+  else {
+    strcpy(retIntervalStr, "Error"); // print error if value is < 0 (this shouldn't ever happen)
+  }
+  
+  char interPelletIntervalStr[10];
+  if (Event != "Pellet" or PelletCount < 2){
+    strcpy(interPelletIntervalStr, "nan"); // print NaN if it's not a pellet Event
+  }
+  else {
+    sprintf(interPelletIntervalStr, "%d", interPelletInterval);
+  }
+      
+  char durationStr[10];
+  if (Event == "Pellet"){
+    strcpy(durationStr, "nan");
+  }
+  else if (Left) {  
+    sprintf(durationStr, "%.2f", leftInterval/1000.0);
+  }
+
+  else if (Right) {
+    sprintf(durationStr, "%.2f", rightInterval/1000.0);
+  }
+
+  int i = 0;
+  char VERchar[10];
+  for (i = 0; i < 5; i++) {
+    VERchar[i] = VER[i];
+  }
+  VERchar[i] = '\0';
+  
+  char sessionchar[sessiontype.length()];
+  for (i = 0; i < sessiontype.length(); i++) {
+    sessionchar[i] = sessiontype[i];
+  }
+  sessionchar[i] = '\0';
+  
+  char EventChar[Event.length()];
+  for (i = 0; i < Event.length(); i++) {
+    EventChar[i] = Event[i];
+  }
+  EventChar[i] = '\0';
+
+  sprintf(s, "%02d/%02d/%04d %02d:%02d:%02d,%s,%s,%d,%.2f,%d,%d,%s,%s,%d,%d,%d,%d,%s,%s,%s\0",
+    now.month(), now.day(), now.year(), now.hour(), now.minute(), now.second(), VERchar, sessionchar, FED, measuredvbat, numMotorTurns+1, FR, EventChar, activePokeStr,
+    LeftCount, RightCount, PelletCount, BlockPelletCount, retIntervalStr, interPelletIntervalStr, durationStr);
+}
+
 //Write to SD card
 void FED3::logdata() {
+  //////////////////////////////////////////////////////
+  //  Creating and sending string to software serial  //
+  //////////////////////////////////////////////////////
+  DateTime now = rtc.now();
+  if (serialOn) {
+    const uint8_t ssize = 100;
+    char s[ssize];
+    writeDataString(s, now);
+    serial.begin(serialSpeed);
+    serial.println(s);
+    serial.end();
+  }
   digitalWrite (MOTOR_ENABLE, LOW);  //Disable motor driver and neopixel
   SD.begin(cardSelect, SD_SCK_MHZ(4));
   
@@ -918,7 +1000,6 @@ void FED3::logdata() {
   /////////////////////////////////
   // Log data and time 
   /////////////////////////////////
-  DateTime now = rtc.now();
   logfile.print(now.month());
   logfile.print("/");
   logfile.print(now.day());
@@ -1563,4 +1644,29 @@ void FED3::writeFEDmode() {
   stopfile.println(timedEnd);
   stopfile.flush();
   stopfile.close();
+}
+
+/******************************************************************************************************************************************************
+                                                                                           Software Serial functions
+******************************************************************************************************************************************************/
+
+void FED3::setSerial(bool b) {
+  serialOn = b;
+}
+
+void FED3::sendJamAlert(){
+  char s[10];
+  sprintf(s, "%d,jam\0", FED);
+  serial.begin(serialSpeed);
+  serial.println(s);
+  serial.end();
+}
+
+void FED3::jamAlertUpdate(){
+  DateTime now = rtc.now();
+  uint32_t diff = now.secondstime() - jamTimer.secondstime();
+  if (diff >= jamAlertInterval){
+    sendJamAlert();
+    jamTimer = DateTime(now);
+  }
 }

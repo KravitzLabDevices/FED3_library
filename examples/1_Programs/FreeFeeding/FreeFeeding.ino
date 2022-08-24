d549b9776bbc67cb74117325808f698ce86983ce
Kravitz Lab <alexxai@wustl.edu>
2022-08-24 11:23:18 -0500

    Update FreeFeeding.ino

--- examples/1_Programs/FreeFeeding/FreeFeeding.ino
+++ examples/1_Programs/FreeFeeding/FreeFeeding.ino
@@ -18,10 +18,10 @@ FED3 fed3 (sketch);                                     //Start the FED3 object
 void setup() {
   fed3.begin();                                         //Setup the FED3 hardware
   fed3.DisplayPokes = false;                            //Customize the DisplayPokes option to 'false' to not display the poke indicators
-  fed3.timeout = 3;                                    //Set a timeout period (in seconds) after each pellet is taken
 }
 
 void loop() {
   fed3.run();                                           //Call fed.run at least once per loop
   fed3.Feed();                                          //Drop pellet
+  fed3.Timeout(5);                                      //5s timeout
 }
